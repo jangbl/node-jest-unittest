aed1bf8813c95132fb097a1cd9753ae06412e893
productioncoder <productioncoding@gmail.com>
2021-05-14 13:23:45 +0200

    update readme + update tests

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Jest Node tutorial: how to use Jest in Node
 
-This tutorial demonstrates how to use [Jest](https://pdfkit.org/) with express.js to perform unit tests
+This tutorial demonstrates how to use [Jest](https://jestjs.io/) with express.js to perform unit tests
 
 <h3 align="center">Please help this repo with a ⭐️ if you find it useful! 😁</h3>
 
-This repository is contains the code for the [Jest Node tutorial video tutorial](https://www.youtube.com/watch?v=A5YiqaQbsyI)
+This repository is contains the code for the [Jest Node tutorial video tutorial](https://www.youtube.com/watch?v=KqQoiFBp36A)
 
-[![Jest Node tutorial.png)](https://www.youtube.com/watch?v=A5YiqaQbsyI)
+[![Jest Node tutorial.png](images/jest-with-node.png)](https://www.youtube.com/watch?v=KqQoiFBp36A)
 
 Please also check out my website at [jangoebel.com](https://jangoebel.com)
 
--- service/__test__/user.test.js
+++ service/__test__/user.test.js
@@ -4,24 +4,24 @@ describe('user service', () => {
   test('does nothing if separation is already correct', () => {
     const { fName, mNames, lName } = userService.sanitizeNames(
       'John',
-      'Alexander',
+      'Alexander Brian',
       'Smith'
     );
 
     expect(fName).toEqual('John');
-    expect(mNames).toEqual('Alexander');
+    expect(mNames).toEqual('Alexander Brian');
     expect(lName).toEqual('Smith');
   });
 
   test('correct separation of firstName if it contains spaces', () => {
     const { fName, mNames, lName } = userService.sanitizeNames(
-      'Jack Dean',
-      'Alexander',
-      'Gates'
+      'John Alexander',
+      'Brian',
+      'Smith'
     );
 
-    expect(fName).toEqual('Jack');
-    expect(mNames).toEqual('Dean Alexander');
-    expect(lName).toEqual('Gates');
+    expect(fName).toEqual('John');
+    expect(mNames).toEqual('Alexander Brian');
+    expect(lName).toEqual('Smith');
   });
 });
